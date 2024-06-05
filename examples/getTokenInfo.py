26025f6beea16cfe2d7cf284c00b6c91fbe04914
1337fin <119663332+1337fin@users.noreply.github.com>
2024-06-05 04:02:14 +0100

    Update getTokenInfo.py

--- examples/getTokenInfo.py
+++ examples/getTokenInfo.py
@@ -2,6 +2,6 @@ from gmgn import gmgn
 
 gmgn = gmgn()
 
-getTokenInfo = gmgn.getTokenInfo("9eLRcHw2G4Ugrnp1p5165PuZsQ2YSc9GnBpGZS7Cpump")
+getTokenInfo = gmgn.getTokenInfo(contractAddress="9eLRcHw2G4Ugrnp1p5165PuZsQ2YSc9GnBpGZS7Cpump")
 
 print(getTokenInfo)
